5422fbbc8058f06eab26edb1ca334fce2034a288
StarryBadger <shaunak.biswas@research.iiit.ac.in>
2023-04-21 12:44:46 +0530

    Added typing effect

--- src/script.js
+++ src/script.js
@@ -3,10 +3,16 @@ function typewriterEffect() {
     const text = element.innerHTML;
     element.innerHTML = "";
     for (let i = 0; i < text.length; i++) {
-      setTimeout(() => {
-        element.innerHTML += text.charAt(i);
-      }, i * 75);
+        setTimeout(() => {
+            element.innerHTML += text.charAt(i);
+        }, i * 75);
     }
-  }
-  window.onload = typewriterEffect;
-  
+}
+window.onload = typewriterEffect;
+function changeColor(element) {
+    if (element.style.color === "pink") {
+        element.style.color = "white";
+    } else {
+        element.style.color = "pink";
+    }
+}
